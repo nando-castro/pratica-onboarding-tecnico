26dbd2c954df1983f467069210e9b5229c394032
nando-castro <fernando.castro10.master@gmail.com>
2022-10-14 16:20:38 -0300

    chore: problem solving

---
+++ drivent__back-end/src/services/enrollments-service/enrollments-service.ts
@@ -0,0 +1,91 @@
+import { prisma } from '@/config';
+
+export async function getOneWithAddressByUserId(userId: number) {
+  const enrollmentWithAddress = await prisma.enrollment.findFirst({
+    where: { userId },
+    include: {
+      Address: true,
+    },
+  });
+
+  if (!enrollmentWithAddress) return undefined;
+
+  const firstAddress = enrollmentWithAddress.Address[0];
+  const address = firstAddress
+    ? {
+        id: firstAddress.id,
+        cep: firstAddress.cep,
+        street: firstAddress.street,
+        city: firstAddress.city,
+        state: firstAddress.state,
+        number: firstAddress.number,
+        neighborhood: firstAddress.neighborhood,
+        addressDetail: firstAddress.addressDetail,
+      }
+    : null;
+
+  return {
+    id: enrollmentWithAddress.id,
+    name: enrollmentWithAddress.name,
+    cpf: enrollmentWithAddress.cpf,
+    birthday: enrollmentWithAddress.birthday,
+    phone: enrollmentWithAddress.phone,
+    ...(!!address && { address }),
+  };
+}
+
+export async function createOrUpdateEnrollmentWithAddress(params: CreateEnrollmentParams) {
+  const createOrUpdateEnrollmentParams = {
+    name: params.name,
+    cpf: params.cpf,
+    birthday: params.birthday,
+    phone: params.phone,
+    userId: params.userId,
+  };
+
+  const enrollment = await prisma.enrollment.upsert({
+    where: {
+      userId: params.userId,
+    },
+    create: createOrUpdateEnrollmentParams,
+    update: createOrUpdateEnrollmentParams,
+    include: {
+      Address: true,
+    },
+  });
+
+  const createOrUpdateAddressParams = {
+    cep: params.address.cep,
+    street: params.address.street,
+    city: params.address.city,
+    number: params.address.number,
+    state: params.address.state,
+    neighborhood: params.address.neighborhood,
+    ...(params.address.addressDetail && { addressDetail: params.address.addressDetail }),
+  };
+
+  await prisma.address.upsert({
+    where: {
+      enrollmentId: enrollment.id,
+    },
+    create: { ...createOrUpdateAddressParams, enrollmentId: enrollment.id },
+    update: createOrUpdateAddressParams,
+  });
+}
+
+export type CreateEnrollmentParams = {
+  name: string;
+  cpf: string;
+  birthday: string;
+  phone: string;
+  userId: number;
+  address: {
+    cep: string;
+    street: string;
+    city: string;
+    number: string;
+    state: string;
+    neighborhood: string;
+    addressDetail?: string;
+  };
+};
